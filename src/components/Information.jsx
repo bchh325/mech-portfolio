12a8175d41e32e5cf5ff0fb0c8982f2c9915c34d
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2023-01-30 03:08:31 -0800

    animation changes

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -4,18 +4,26 @@ import styles from './Information.module.css'
 export default function Information() {
     return (
         <div className={styles.information}>
-            <div className={styles.background}/>
-            <div className={`${styles.infochild} ${styles.delayOne}`}>
+            <div className={styles.background} />
+            <div className={`${styles.infochild} ${styles.hide} ${styles.fadeIn} ${styles.delayOne}`}>
                 <span className={styles.default}>John <span className={styles.highlight}>Doe</span></span>
             </div>
-            <div className={`${styles.infochild} ${styles.delayTwo}`}>
-                <p className={styles.details}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi culpa aliquam necessitatibus, laborum tenetur magnam, excepturi hic voluptate reprehenderit harum a possimus, deserunt delectus assumenda aperiam placeat fuga? Expedita, sit. </p>
+            <div className={`${styles.infochild}`}>
+                <div className={styles.borderLeft}>
+                    <p className={`${styles.details} ${styles.slideText} ${styles.initialTransform} ${styles.delayTwo}`}>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente quos, voluptatibus accusamus nemo impedit praesentium laudantium odio ipsa odit commodi officiis rem aut perferendis? Repellendus pariatur debitis sit hic quia!
+                    </p>
+                </div>
             </div>
-            <div className={`${styles.infochild} ${styles.delayThree}`}>
+            <div className={`${styles.infochild}`}>
+                <div className={styles.borderRight}>
+                    <p className={`${styles.details} ${styles.slideTextAlt} ${styles.initialTransformAlt} ${styles.delayFour}`}>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente quos, voluptatibus accusamus nemo impedit praesentium laudantium odio ipsa odit commodi officiis rem aut perferendis? Repellendus pariatur debitis sit hic quia!
+                    </p>
+                </div>
+            </div>
+            <div className={`${styles.infochild}  ${styles.hide} ${styles.fadeIn} ${styles.delayThree}`}>
                 <span className={styles.default}>Aerospace <span className={styles.highlight}>Engineering</span></span>
-            </div>
-            <div className={`${styles.infochild} ${styles.delayFour}`}>
-                <p className={styles.details}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente quos, voluptatibus accusamus nemo impedit praesentium laudantium odio ipsa odit commodi officiis rem aut perferendis? Repellendus pariatur debitis sit hic quia!</p>
             </div>
         </div>
     )
