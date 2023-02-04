fbe202559bcaabbd275264d6232202ff51798aa0
bchh325 <67843182+bchh325@users.noreply.github.com>
2023-02-03 23:52:29 -0800

    project styles

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,21 +4,23 @@ import styles from './Project.module.css'
 export default function Project() {
   return (
     <div className={styles.project}>
-        <div className={styles.info}>
-            <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo corrupti sapiente totam dolorem ratione laborum ab labore excepturi? Aspernatur eaque a unde eveniet natus obcaecati dignissimos aperiam corporis beatae! Corporis?
-            </p>
-            <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo corrupti sapiente totam dolorem ratione laborum ab labore excepturi? Aspernatur eaque a unde eveniet natus obcaecati dignissimos aperiam corporis beatae! Corporis?
-            </p>
-            <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo corrupti sapiente totam dolorem ratione laborum ab labore excepturi? Aspernatur eaque a unde eveniet natus obcaecati dignissimos aperiam corporis beatae! Corporis?
-            </p>
-            <span>Span</span>
-            <span>Span</span>
-            <span>Span</span>
-            <span>Span</span>
+      <div className={styles.info}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo corrupti sapiente totam dolorem ratione laborum ab labore excepturi? Aspernatur eaque a unde eveniet natus obcaecati dignissimos aperiam corporis beatae! Corporis?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo corrupti sapiente totam dolorem ratione laborum ab labore excepturi? Aspernatur eaque a unde eveniet natus obcaecati dignissimos aperiam corporis beatae! Corporis?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo corrupti sapiente totam dolorem ratione laborum ab labore excepturi? Aspernatur eaque a unde eveniet natus obcaecati dignissimos aperiam corporis beatae! Corporis?
+        </p>
+        <div className={styles.links}>
+          <span>Span</span>
+          <span>Span</span>
+          <span>Span</span>
+          <span>Span</span>
         </div>
+      </div>
     </div>
   )
 }
